5d0cf11a61414b57c477372de3d8379780aba844
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-07-21 09:55:23 -0500

    Add library folder with data of 1 team

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const fetch = require('node-fetch')
 
 fetch('https://statsapi.web.nhl.com/api/v1/teams')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => console.log(data.teams[0]));
